cf8a552f5327e035a56fcb8ecc2a29e638fcd6b2
Apollo Akora <apolloakora@gmail.com>
2019-03-15 14:37:32 +0000

    set configuration directives use case started

--- lib/usecase/set.rb
+++ lib/usecase/set.rb
@@ -2,39 +2,37 @@
 	
 module SafeDb
 
-  # The <b>set <em>use case</em></b> is the generic tool for setting configuration
-  # directives inside the safe workstation INI formatted file.
+  # The <b>set <em>use case</em></b> is the generic tool for setting book scoped
+  # configuration directives. These directives can only be read, written, updated
+  # or removed during a logged in session.
   #
   # The mirror of this use case is <b><em>unset</em></b>.
   #
   # == Observable Value
   #
-  # The configuration directive will eithe be created (or will overwrite) an existing
-  # directive with the same path.
-  #
-  # The configuration file is printed to inform the user of the current state.
-  #
-  # == Alternative / Error Flows
-  #
-  # Error - if the directive path is not composed of two (fwd slash separated) parts
-  # Error - if the directive path and/or value contains (or not) unacceptable characters
+  # The configuration directive will either be created or overwriten within the
+  # book's configuration store.
   #
   class Set < UseCase
 
-    attr_writer :domain_name
+    attr_writer :directive_name, :directive_value
 
-
-    # The <b>use <em>use case</em></b> is borrowed from the database world and it denotes
-    # the domain to be used <b>for now (and evermore)</b> for this workstation until another
-    # use command is issued.
-    #
-    # The parameter domain_name must be set after an object instance is acquired but
-    # before the execute method runs.
+    # The <b>set <em>use case</em></b> is the generic tool for setting book scoped
+    # configuration directives. These directives can only be read, written, updated
+    # or removed during a logged in session.
     def execute
-    end
 
+      return unless ops_key_exists?
+      master_db = KeyApi.read_master_db()
 
-    def pre_validation
+      master_db[ @directive_name ] = @directive_value
+
+      puts ""
+      puts JSON.pretty_generate( master_db )
+      puts ""
+
+      KeyApi.write_master_db( create_header(), master_db )
+
     end
 
 
