2327d2cd1716f28903315208fe93614966593f04
Apollo Akora <apolloakora@gmail.com>
2019-03-27 13:44:42 +0000

    removing inspect code from show

--- lib/usecase/show.rb
+++ lib/usecase/show.rb
@@ -26,69 +26,6 @@ module SafeDb
       has_verse = has_chapter && chapter_data.has_key?( master_db[ KEY_PATH ] )
 
 
-##global_variables - DONE
-##local_variables - DONE
-##instance_variables - DONE
-##class_variables - tough nut to crack with very little benefit (method class_variables not defined)
-
-      puts ""
-      puts "QQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQ ~~~~~~~~~~~~~ Global Variable Array List ~~~~~~~~~~~~~~~~ QQQQQ"
-      puts "QQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-
-      puts global_variables().inspect
-
-      puts "QQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQ ~~~~~~~~~~~~~ Global Variable Values Printed ~~~~~~~~~~~~~~~~ QQQQQ"
-      puts "QQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-
-      global_variables().sort.each do |name|
-
-        puts "<<< ------------------------------------------------------------------->>>"
-        puts "<<< #{name.to_s} >>>"
-        puts "<<< ------------------------------------------------------------------->>>"
-        next if name.to_s.eql?( "$FILENAME" )
-        global_variable_value = eval "#{name}.inspect"
-        puts "<<< #{global_variable_value}"
-
-      end
-
-      puts ""
-      puts "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts ""
-      puts "QQQQQQQQQQQ QQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQQQQQQQQQ Bug Finder QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQQQQQQQQQ QQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts ""
-      self.instance_variables().map do |attribute|
-        puts "=============================================="
-        puts "----------------------------------------------"
-        puts attribute
-        pp self.instance_variable_get(attribute)
-      end
-      puts "=============================================="
-      puts "QQQQQQQQQQQ QQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQQQQQQQQQ QQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts ""
-      puts "### ------------------------------------"
-      puts "### Inspect View"
-      puts "### ------------------------------------"
-      pp self.inspect
-      puts "### ------------------------------------"
-      puts "QQQQQQQQQQQ QQQQQQQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQQQQQQQQQ Local Variables QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-      puts "QQQQQQQQQQQ QQQQQQQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-
-      local_variables().map do |attribute|
-        puts "=============================================="
-        puts "----------------------------------------------"
-        puts attribute
-        pp binding().local_variable_get(attribute.to_sym)
-      end
-      puts "QQQQQQQQQQQ QQQQQQQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
-
-      puts ""
 
 
       return unless has_verse
