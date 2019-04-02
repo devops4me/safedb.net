249c9f96780c9f50f2e8635cc1d3f44fa2f93ec4
Apollo Akora <apolloakora@gmail.com>
2019-04-02 23:40:54 +0100

    moving book related use cases to book folder

--- lib/usecase/book/import.rb
+++ lib/usecase/book/import.rb
@@ -124,33 +124,6 @@ module SafeDb
     end
 
 
-    private
-
-
-    def print_put_success
-
-      puts ""
-      puts "Success putting a key/value pair into the open envelope."
-      puts "You can put more in and then close the envelope."
-      puts ""
-      puts "    #{COMMANDMENT} close"
-      puts ""
-
-    end
-
-
-    # Perform pre-conditional validations in preparation to executing the main flow
-    # of events for this use case. This method may throw the below exceptions.
-    #
-    # @raise [SafeDirNotConfigured] if the safe's url has not been configured
-    # @raise [EmailAddrNotConfigured] if the email address has not been configured
-    # @raise [StoreUrlNotConfigured] if the crypt store url is not configured
-    def pre_validation
-
-
-    end
-
-
   end
 
 
