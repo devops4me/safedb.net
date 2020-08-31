79770f15d952d9192eafec2e3de968840ebcf839
Migrate Workshop <workshop.ah7@gmail.com>
2020-08-31 23:12:28 +0100

    added step definition with exception assertion

--- features/support/access-step-defns.rb
+++ features/support/access-step-defns.rb
@@ -9,6 +9,12 @@ When(/^I login to book "(.*?)" with password "(.*?)"$/) do |book_name, book_pass
   CLI.start([ "login", book_name, "--password=#{book_password}" ] )
 end
 
-When(/^I view the book$/) do
-  CLI.start([ "view" ] )
+When(/^I should be logged in to book "(.*?)"$/) do |book_name|
+
+  book_id = SafeDb::Identifier.derive_ergonomic_identifier( book_name, SafeDb::Indices::SAFE_BOOK_ID_LENGTH )
+  unless SafeDb::StateInspect.is_logged_in?(book_id)
+    error_message = "Expected to be logged into book #{book_name} with id #{book_id}"
+    raise RuntimeError, error_message
+  end
+
 end
